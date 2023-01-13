c0a1b787e13e19cbc4e92017cd44305e5aa464ae
Mason719070 <122560385+Mason719070@users.noreply.github.com>
2023-01-13 13:23:47 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.AB, function () {
+    music.playSoundEffect(music.createSoundEffect(
+    WaveShape.Sine,
+    5000,
+    5000,
+    255,
+    0,
+    500,
+    SoundExpressionEffect.None,
+    InterpolationCurve.Linear
+    ), SoundExpressionPlayMode.UntilDone)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.SmallHeart)
+})
+basic.showIcon(IconNames.Angry)
