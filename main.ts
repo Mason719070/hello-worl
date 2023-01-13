23d2cd3a2d6fcc526e32a183313cc64e8a1fef4f
Mason719070 <122560385+Mason719070@users.noreply.github.com>
2023-01-13 13:25:55 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,16 +2,7 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Heart)
 })
 input.onButtonPressed(Button.AB, function () {
-    music.playSoundEffect(music.createSoundEffect(
-    WaveShape.Sine,
-    5000,
-    5000,
-    255,
-    0,
-    500,
-    SoundExpressionEffect.None,
-    InterpolationCurve.Linear
-    ), SoundExpressionPlayMode.UntilDone)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.SmallHeart)
